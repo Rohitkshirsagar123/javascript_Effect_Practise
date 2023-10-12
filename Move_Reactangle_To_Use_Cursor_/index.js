d1300a2a143b176525bdb0cfccb527dae460572b
unknown <rogitkshirsagar262@gmail.com>
2023-10-12 14:33:32 +0530

    changes in js file

--- Move_Reactangle_To_Use_Cursor_/index.js
+++ Move_Reactangle_To_Use_Cursor_/index.js
@@ -2,7 +2,6 @@
 var rect=document.querySelector("#rect");
 
 window.addEventListener("mousemove",function(details){
-    // console.log(details.clientX);
     var xval = gsap.utils.mapRange(0,window.innerWidth,100+rect.getBoundingClientRect().width/2,
                                     window.innerWidth-(100+rect.getBoundingClientRect().width/2),details.clientX)
     gsap.to("#rect", {
